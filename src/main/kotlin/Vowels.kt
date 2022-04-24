def789e780540fa48919b4c384f5f76bb8fbe0e1
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-04-25 00:21:59 +0200

    vowels regex solution

--- src/main/kotlin/Vowels.kt
+++ src/main/kotlin/Vowels.kt
@@ -11,8 +11,18 @@ fun vowels(str: String): Int {
     }
 }
 
+fun vowelsRegex(str: String): Int {
+    return "[aeiou]".toRegex(RegexOption.IGNORE_CASE).findAll(str).count()
+}
+
+
 fun main() {
+    vowelsRegex("a abe")
     Assertions.assertTrue(vowels("aeiou") == 5)
     Assertions.assertTrue(vowels("Hallo") == 2)
     Assertions.assertTrue(vowels("HALLO WALLI AND EVA") == 7)
+
+    Assertions.assertTrue(vowelsRegex("aeiou") == 5)
+    Assertions.assertTrue(vowelsRegex("Hallo") == 2)
+    Assertions.assertTrue(vowelsRegex("HALLO WALLI AND EVA") == 7)
 }
