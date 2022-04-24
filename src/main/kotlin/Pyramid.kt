0b42c5a9666830e8431541d55f39b6744a62e757
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-04-25 00:09:21 +0200

    pyramid solution

--- src/main/kotlin/Pyramid.kt
+++ src/main/kotlin/Pyramid.kt
@@ -7,14 +7,17 @@
 // "#######"
 
 fun main() {
-    pyramid(10)
+    pyramid(4)
 }
 
 fun pyramid(levels: Int) {
     val levelSize = levels * 2 - 1
-    for (i in 1.. levelSize) {
-        println(" ".repeat(levelSize - i)
-                + "#".repeat(i*2-1)
-                + " ".repeat(levelSize - i))
+    for (i in 1.. levels) {
+        val blocksAtCurrentLevel = i*2-1
+        val border = (levelSize - blocksAtCurrentLevel) / 2
+        println(" ".repeat(border)
+                + "#".repeat(blocksAtCurrentLevel)
+                + " ".repeat(border)
+        )
     }
 }
